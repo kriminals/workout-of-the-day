4f90eeb6efd5537120ac93f7d29be5e0d8b0fc4a
George Tzinetas <krimini.kriminals@gmail.com>
2021-02-16 09:26:01 +0200

    Fetch multiple wod Days

--- pageController.js
+++ pageController.js
@@ -1,10 +1,29 @@
+const {addDays, isBefore, isWeekend, format} = require('date-fns');
+
 const pageScraper = require('./pageScraper');
+
 async function scrapeAll(browserInstance){
     let browser;
+    // Quarantine Day 1
+    let qd1 = new Date('2020-11-2');
+    let today = new Date('2020-11-7');
+    // Fill wodDays from quarantine Day 1 untill today, except weekends
+    let currentDay = qd1;
+    let wodDays = [];
+    while (isBefore(currentDay, today)) {
+        if (!isWeekend(currentDay)) wodDays.push(format(currentDay, 'd-M-yyyy'));
+        currentDay = addDays(currentDay, 1);
+    };
+    console.log(wodDays);
+    let startDate = format(qd1, 'd-M-yyyy');
+    let endDate = format(today, 'd-M-yyyy'); 
     try{
         browser = await browserInstance;
-        await pageScraper.scraper(browser);
-
+        await wodDays.reduce(async (acc, date) => {
+            await pageScraper.scraper(browser, date);
+            //await browser.close();
+        }, undefined )
+        
     }
     catch(err){
         console.log("Could not resolve the browser instance => ", err);
--- pageScraper.js
+++ pageScraper.js
@@ -1,12 +1,17 @@
 const scraperObject = {
-    url: 'https://www.boosterfitness.gr/12-2-2021/',
-    async scraper(browser){
+    url: 'https://www.boosterfitness.gr/',
+    async scraper(browser, date){
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
-        await page.goto(this.url);
+        await page.goto(`${this.url}/${date}/`, {
+            waitUntil: 'load',
+            // Remove the timeout
+            timeout: 0
+        });
         await page.waitForSelector('.content');
         let wod = await page.$$eval('div.content p', exercises => exercises.map(p => p.textContent));
         console.log(wod);
+        await page.close();
 
     }
 }
